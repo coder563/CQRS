229ecae97d043293bf631265e1d1a17873439882
N M <walkerml563@gmail.com>
2023-04-18 05:02:51 +0100

    added aggregate root

--- SM-Post/Post.Cmd/Post.Cmd.Api/Events/PostCreatedEvent.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Events/PostCreatedEvent.cs
@@ -7,7 +7,14 @@ namespace Post.Cmd.Api.Events  {
 
     public class PostCreatedEvent:BaseEvent{
 
-            protected PostCreatedEvent():base(nameof(PostCreatedEvent)){
+            public PostCreatedEvent():base(nameof(PostCreatedEvent)){}
+            public string Author{get;set;}
+
+            public string Message{get;set;}
+            public DateTime DatePosted{get;set;}
+
+
+
 
 
                 
@@ -22,5 +29,3 @@ namespace Post.Cmd.Api.Events  {
 
 
 
-
- }
