41bd7fb6ba87651e6685f12d7cfe7f1b2ffdf6c3
N M <walkerml563@gmail.com>
2023-04-19 06:25:53 +0100

    added events and postaggregate

---
+++ SM-Post/Post.Cmd/Post.Cmd.Domain/Aggregates/PostAggregate.cs
@@ -0,0 +1,73 @@
+
+
+using CQRS.Core.Domain;
+using Post.Common.Events;
+
+namespace Post.Cmd.Domain{
+
+        public class PostAggregate:AggregateRoot{
+
+
+            private bool _active;
+            private string _author;
+
+            private readonly Dictionary<Guid, Tuple<string,string>> _comments = new();
+
+
+            public bool Active {
+
+                get =>_active  ; set =>_active=value;
+            }
+
+            public PostAggregate(Guid id, string author, string message){
+
+                RaiseEvent(new PostCreatedEvent{
+
+                    Id = id,
+                    Author = author,
+                    Message = message,
+                    DatePosted = DateTime.Now
+                                        
+                });
+            }
+            
+            public void Apply(PostCreatedEvent @event){
+
+                    _id = @event.Id;
+                    _active = true;
+                    _author = @event.Author;
+            }
+
+            public void EditMessage(string message){
+
+                    if(_active==false){
+
+                            throw new InvalidOperationException("You cannot edit message of an inactive post");
+                    }else if (string.IsNullOrWhiteSpace(message)){
+
+                            throw new InvalidOperationException($"The value of {nameof(message)} is either blank or null");
+                    }
+
+
+            }
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+}
+
+}
---
+++ SM-Post/Post.Common/Events/CommentUpdatedEvent.cs
@@ -0,0 +1,10 @@
+
+
+
+namespace Post.Common.Events{
+
+
+
+
+    
+}
